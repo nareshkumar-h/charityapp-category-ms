64320d61f6f562bf96087079a5c4afd7203e295e
shalujose <shalujosem0@gmail.com>
2019-10-25 17:53:40 +0530

    resource changes committed

--- src/main/java/com/revature/charityappcategoryms/config/CorsConfiguration.java
+++ src/main/java/com/revature/charityappcategoryms/config/CorsConfiguration.java
@@ -10,7 +10,7 @@ public class CorsConfiguration implements WebMvcConfigurer {
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 
-		registry.addMapping("/**").allowedOrigins("http://localhost:8080")
+		registry.addMapping("/**").allowedOrigins("http://localhost:4200")
 				.allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "HEAD").allowCredentials(true);
 	}
 }
